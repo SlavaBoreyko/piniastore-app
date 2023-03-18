651304ada7ad81657a4101403c3010f0c9d2c1fe
yaroslavboreyko <yaroslav.boreyko@gmail.com>
2023-03-18 20:40:22 +0100

    Displaying the Cart Items. Clear button for reset Store.

--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -1,10 +1,17 @@
 import { defineStore } from "pinia";
+import { groupBy } from "lodash";
 
 export const useCartStore = defineStore("CartStore", {
   state: () => {
     return {
       items: [],
     };
+  },
+  getters: {
+    count: (state) => state.items.length,
+    isEmpty: (state) => state.count === 0,
+    grouped: (state) => groupBy(state.items, (item) => item.name),
+    groupCount: (state) => (name) => state.grouped[name].length,
   },
   actions: {
     addItems(count, item) {
