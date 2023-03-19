f03c606add1ee0f927cf2062933a4ec597b757f8
yaroslavboreyko <yaroslav.boreyko@gmail.com>
2023-03-19 10:01:33 +0100

    Redo button with future push functionality.

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,22 @@ import { useCartStore } from "./stores/CartStore";
 const cartStore = useCartStore();
 const productStore = useProductStore();
 const history = reactive([]);
+const future = reactive([]);
 const doingHistory = ref(false);
-
 history.push(JSON.stringify(cartStore.$state));
+
+const redo = () => {
+  const latestState = future.pop();
+  if (!latestState) return;
+  doingHistory.value = true;
+  history.push(latestState);
+  cartStore.$state = JSON.parse(latestState);
+  doingHistory.value = false;
+};
 const undo = () => {
   if (history.length === 1) return;
   doingHistory.value = true;
-  history.pop();
+  future.push(history.pop());
   cartStore.$state = JSON.parse(history.at(-1));
   doingHistory.value = false;
 };
@@ -22,6 +31,7 @@ const undo = () => {
 cartStore.$subscribe((mutation, state) => {
   if (!doingHistory.value) {
     history.push(JSON.stringify(state));
+    future.splice(0, future.length);
   }
 });
 
@@ -40,6 +50,7 @@ productStore.fill();
     <TheHeader />
     <div class="mb-5 flex justify-end">
       <AppButton @click="undo">Undo</AppButton>
+      <AppButton class="ml-2" @click="redo">Redo</AppButton>
     </div>
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
