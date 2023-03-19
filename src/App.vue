e28781ef3ba760ab44505902c25a132f25beda39
yaroslavboreyko <yaroslav.boreyko@gmail.com>
2023-03-19 09:10:10 +0100

    Undo button for Cart History.Subscribe to the state.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,46 @@
 <script setup>
+import { ref, reactive } from "vue";
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
 import { useProductStore } from "./stores/ProductStore";
 import { useCartStore } from "./stores/CartStore";
+
 const cartStore = useCartStore();
 const productStore = useProductStore();
+const history = reactive([]);
+const doingHistory = ref(false);
+
+history.push(JSON.stringify(cartStore.$state));
+const undo = () => {
+  if (history.length === 1) return;
+  doingHistory.value = true;
+  history.pop();
+  cartStore.$state = JSON.parse(history.at(-1));
+  doingHistory.value = false;
+};
+
+cartStore.$subscribe((mutation, state) => {
+  if (!doingHistory.value) {
+    history.push(JSON.stringify(state));
+  }
+});
+
+cartStore.$onAction(({ name, store, args, after, onError }) => {
+  if (name === "addItems") {
+    after(() => {
+      console.log(args[0]);
+    });
+  }
+});
 productStore.fill();
 </script>
 
 <template>
   <div class="container">
     <TheHeader />
+    <div class="mb-5 flex justify-end">
+      <AppButton @click="undo">Undo</AppButton>
+    </div>
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
         v-for="product in productStore.products"
--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -1,4 +1,4 @@
-import { defineStore } from "pinia";
+import { defineStore, acceptHMRUpdate } from "pinia";
 import { groupBy } from "lodash";
 
 export const useCartStore = defineStore("CartStore", {
@@ -10,8 +10,15 @@ export const useCartStore = defineStore("CartStore", {
   getters: {
     count: (state) => state.items.length,
     isEmpty: (state) => state.count === 0,
-    grouped: (state) => groupBy(state.items, (item) => item.name),
+    grouped: (state) => {
+      const grouped = groupBy(state.items, (item) => item.name);
+      const sorted = Object.keys(grouped).sort();
+      let inOrder = {};
+      sorted.forEach((key) => (inOrder[key] = grouped[key]));
+      return inOrder;
+    },
     groupCount: (state) => (name) => state.grouped[name].length,
+    total: (state) => state.items.reduce((p, c) => p + c.price, 0),
   },
   actions: {
     addItems(count, item) {
@@ -20,5 +27,16 @@ export const useCartStore = defineStore("CartStore", {
         this.items.push({ ...item });
       }
     },
+    clearItem(itemName) {
+      this.items = this.items.filter((item) => item.name !== itemName);
+    },
+    setItemCount(item, count) {
+      this.clearItem(item.name);
+      this.addItems(count, item);
+    },
   },
 });
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useCartStore, import.meta.hot));
+}
